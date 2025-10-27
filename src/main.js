fd052e01bb41d04d5b31b3881d93c2ed8243c560
Sean Barkley <destroyerdust@gmail.com>
2025-10-26 23:08:17 -0400

    chore(analytics): add analytics tracking to main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import './assets/main.css'
 import { createApp } from 'vue'
-import { Analytics } from '@vercel/analytics/vue'
+import { inject } from '@vercel/analytics'
 import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(Analytics)
+inject()
 
 app.mount('#app')
